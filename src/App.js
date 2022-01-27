8243b67eac6ec2b95d2f8d0e49ca6eb9e7f59985
joeys1122 <josephs1122@gmail.com>
2022-01-27 16:26:55 -0500

    random quote gen added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,7 @@
+import React from 'react';
+
+import Quote from './components/Quote';
+
 import './App.css';
 
 function App() {
@@ -7,6 +11,7 @@ function App() {
         <h1>Random Quote Generator</h1>
         <span>Built using <a href='https://rapidapi.com/martin.svoboda/api/quotes15/details'>Quotes API </a>by Martin Svoboda</span>
       </div>
+      <Quote/>
     </div>
   );
 }
